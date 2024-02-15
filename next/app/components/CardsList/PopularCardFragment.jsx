983d110d71923cdc652fb1af59db99e90dff129d
maxz2024 <maksimzdonov456@yandex.ru>
2024-02-16 00:58:27 +0300

    create 4 files and update 5 files update 6 files

---
+++ next/app/components/CardsList/PopularCardFragment.jsx
@@ -0,0 +1,27 @@
+import { Card } from "./Card"
+
+export const PopularCardFragment = () => {
+    return (
+    <>
+        <Card url="https://code.s3.yandex.net/teens/pindie-games/cristal-keeper/cover.png"
+            name="Crystal Kepper"
+            description="Управляй боевым дроном, чтобы любой ценой защитить кристалл от враждебных космо-слизней."
+            author_name="Lonely Baobab"
+            voice="20" />
+        <Card url="https://code.s3.yandex.net/teens/pindie-games/dangeons-n-caves-prologue/cover.png"
+            name="Dangeons'n'Caves. Prologue"
+            description="Безымянный герой исследует пещеры и подземелья, чтобы найти
+            больше информации о себе."
+            author_name="F-Style"
+            voice="10"
+        />
+        <Card url="https://code.s3.yandex.net/teens/pindie-games/defence-of-crystal/cover.png"
+            name="Defence of Crystal"
+            description="Защищай магический кристалл от монстров и уничтожай кладбища,
+            чтобы спасти Землю, которую поглотил мрак."
+            author_name="MastWe"
+            voice="20"
+        />
+    </>
+    )
+}
--- next/app/components/Footer/Footer.jsx
+++ next/app/components/Footer/Footer.jsx
@@ -1,31 +1,3 @@
-/* export const Footer = () => {
-  return (
-    <footer className="footer">
-      <a href="./index.html" className="footer__logo">
-        <span className="footer__logo-name">pindie</span>
-        <span className="footer__logo-copy">, XXI век</span>
-      </a>
-      <ul className="social-list">
-        <li className="social-list__item">
-          <a href="" className="button social-list__link">
-            YT
-          </a>
-        </li>
-        <li className="social-list__item">
-          <a href="" className="button social-list__link">
-            ВК
-          </a>
-        </li>
-        <li className="social-list__item">
-          <a href="https://t.me/maxz2024" className="button social-list__link">
-            TG
-          </a>
-        </li>
-      </ul>
-    </footer>
-  );
-};
- */
 import Styles from './Footer.module.css'
 
 export const Footer = () => {
