12988355c3c3057c1fceaee1d3547a060a2f935a
maxz2024 <maksimZdonov456@yandex.ru>
2025-03-16 12:24:19 +0000

    Очистка кеша и обновление .gitignore

--- app/api/config.js
+++
@@ -1,9 +0,0 @@
-export const BASE_URL = "https://backend.pindie.my-kinozal.ru/api";
-
-export const endpoints = {
-  games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/login`,
-  users: `${BASE_URL}/users`,
-  getMe: `${BASE_URL}/me`,
-  checkExists: `${BASE_URL}/users/check-exist`
-};
